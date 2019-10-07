2a6639cc018137d4e42d889a276e42b39ac4dbee
rudiq4 <rudikvovan@gmail.com>
2019-10-08 00:17:14 +0300

    basement

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Category, Brand, VehicleType, Vehicle
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['category', 'title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class VehicleTypeAdmin(admin.ModelAdmin):
+    list_display = ['brand', 'title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ['category', 'brand', 'vehicle_type']
+
+
+
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = [
+#         'name', 'slug', 'price',
+#         'available', 'created', 'updated'
+#     ]
+#     list_editable = [
+#         'price', 'available'
+#     ]
+#     prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(VehicleType, VehicleTypeAdmin)
+admin.site.register(Vehicle, VehicleAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=32, verbose_name='Категорія', db_index=True)
+    slug = models.SlugField(max_length=32, db_index=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Категорія'
+        verbose_name_plural = 'Категорії'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        pass
+        # return reverse('shop:ProductListByCategory', args=[self.slug])
+
+
+class Brand(models.Model):
+    category = models.ForeignKey(Category, verbose_name='Категорія', on_delete=models.CASCADE)
+    title = models.CharField(max_length=32, verbose_name='Виробник', db_index=True)
+    slug = models.SlugField(max_length=32, db_index=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Марка'
+        verbose_name_plural = 'Марка'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        pass
+        # return reverse('shop:ProductListByCategory', args=[self.slug])
+
+
+class VehicleType(models.Model):
+    brand = models.ForeignKey(Brand, verbose_name='Виробник', on_delete=models.CASCADE)
+    title = models.CharField(max_length=32, verbose_name='Модель', db_index=True)
+    slug = models.SlugField(max_length=32, db_index=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Модель'
+        verbose_name_plural = 'Моделі'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        pass
+        # return reverse('shop:ProductListByCategory', args=[self.slug])
+
+
+class Vehicle(models.Model):
+    category = models.ForeignKey(Category, verbose_name='Категорія', on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, verbose_name='Марка', on_delete=models.CASCADE)
+    vehicle_type = models.ForeignKey(VehicleType, verbose_name='Модель', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Автомобіль'
+        verbose_name_plural = 'Автомобілі'
+
+    def __str__(self):
+        return '{} {}'.format(self.brand, self.vehicle_type)
+
+    def get_absolute_url(self):
+        pass
+        # return reverse('shop:ProductListByCategory', args=[self.slug])
