1b6c67a72c142db1f1ccd7e92372987cf9aafed3
Orest <oresthaponov@gmail.com>
2019-10-10 15:45:32 +0300

    login popup

--- main/static/JS/main.js
+++ main/static/JS/main.js
@@ -3,3 +3,23 @@ $(document).ready(function () {
         $(".header__responsive__main").fadeToggle(500);
     });
 });
+
+$(document).ready(function () {
+    $('.login_btn').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#name',
+
+        // When elemened is focused, some mobile browsers in some cases zoom in
+        // It looks not nice, so we disable it:
+        callbacks: {
+            beforeOpen: function () {
+                if ($(window).width() < 700) {
+                    this.st.focus = false;
+                } else {
+                    this.st.focus = '#name';
+                }
+            }
+        }
+    });
+});
